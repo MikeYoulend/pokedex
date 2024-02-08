dc97c047d80d33fba5c263b97744cc4888e87171
MikeYoulend <mikeyoulend@gmail.com>
2024-02-08 13:57:17 +0100

    Pokedex React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import Main from "./Components/Main";
+import "./Components/style.css";
 
 function App() {
-	return <div className="App"></div>;
+	return <Main />;
 }
 
 export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import charmanderImage from "../images/charmander.png";
+
+const Card = () => {
+	return (
+		<div className="card">
+			<h2>1</h2>
+			<img src={charmanderImage} alt="" />
+			<h2>Charmander</h2>
+		</div>
+	);
+};
+
+export default Card;
